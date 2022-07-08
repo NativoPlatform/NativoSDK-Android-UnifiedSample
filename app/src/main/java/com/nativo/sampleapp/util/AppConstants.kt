2e77b83466c7cd7a712ea75ab00a49e35102bf04
Kou Thao <kthao@nativo.com>
2022-07-08 10:55:56 -0700

    Upgrade gradle version, import kotlin, convert `AppConstants` to kotlin

---
+++ app/src/main/java/com/nativo/sampleapp/util/AppConstants.kt
@@ -0,0 +1,23 @@
+package com.nativo.sampleapp.util
+
+/**
+ * Contains constant variables
+ */
+object AppConstants {
+    const val SECTION_URL = "http://www.nativo.net/test/"
+    const val DFP_SECTION_URL = "http://www.nativo.net/mobiledfptest"
+    const val MOAP_SECTION_URL = "http://www.nativo.net/moap?ntv_pl=1099995"
+    const val PUBLISHER_URL = "http://dsp.test-sites.internal.nativo.net/external/mobile_web.html"
+    const val CLICK_OUT_URL = "https://www.nativo.com/"
+    const val SP_CAMPAIGN_ID = "CAMPAIGN_ID"
+    const val SP_SECTION_URL = "SECTION_URL"
+    const val SP_CONTAINER_HASH = "CONTAINER_HASH"
+
+    //
+    const val SAMPLE_GDPR_CONSENT = "BOqm1JLOqm1JLAPABBAAABAAAAAsuABgACBZYA"
+
+    //changed last few chars of valid string
+    const val SAMPLE_GDPR_INVALID_CONSENT = "BOqm1JLOqm1JLAPABBAAABAAAAAsuABgA"
+    const val SAMPLE_CCPA_VALID_CONSENT = "1YNY"
+    const val SAMPLE_CCPA_INVALID_CONSENT = "1YYY"
+}
