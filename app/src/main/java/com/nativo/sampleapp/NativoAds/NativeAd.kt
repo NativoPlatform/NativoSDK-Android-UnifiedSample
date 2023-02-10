1b143f6b9ed9233590c309665bf480539d8fd65d
Matt Murray <mattm.trinsic@gmail.com>
2023-02-10 13:25:32 -0800

    Add new template for native

--- app/src/main/java/com/nativo/sampleapp/NativoAds/NativeAd.kt
+++ app/src/main/java/com/nativo/sampleapp/NativoAds/NativeAd.kt
@@ -1,10 +1,8 @@
 package com.nativo.sampleapp.NativoAds
 
 import android.content.Context
-import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
-import androidx.cardview.widget.CardView
 import android.widget.TextView
 import com.nativo.sampleapp.R
 import net.nativo.sdk.injectable.NtvNativeAdInjectable
@@ -26,34 +24,23 @@ class NativeAd : NtvNativeAdInjectable {
 
     // Misc
     private var sponsoredTag: TextView? = null
-    private var cardView: CardView? = null
 
     override fun getLayout(context: Context): Int {
-        return R.layout.native_article
+        return R.layout.native_article_2
     }
 
     override fun bindViews(v: View) {
         view = v
-        cardView = v.findViewById(R.id.article_constraint_layout)
         titleLabel = v.findViewById(R.id.article_title)
         authorLabel = v.findViewById(R.id.article_author)
         previewImageView = v.findViewById(R.id.article_image)
-        //dateLabel = v.findViewById(R.id.article_date)
         previewTextLabel = v.findViewById(R.id.article_description)
         authorImageView = v.findViewById(R.id.article_author_image)
         sponsoredTag = v.findViewById(R.id.sponsored_tag)
         adChoicesImageView = v.findViewById(R.id.adchoices_indicator)
     }
 
-    // Use this to toggle between regular article and nativo sponsored ad
     override fun displaySponsoredIndicators(isSponsored: Boolean) {
-        if (isSponsored) {
-            cardView?.setBackgroundColor(Color.LTGRAY)
-            sponsoredTag?.visibility = View.VISIBLE
-        } else {
-            cardView?.setBackgroundColor(Color.WHITE)
-            sponsoredTag?.visibility = View.INVISIBLE
-        }
     }
 
     override fun formatDate(date: Date?): String? {
